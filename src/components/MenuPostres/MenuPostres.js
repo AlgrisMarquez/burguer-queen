a56106c537ee00c9410db16ec357954603216627
AlgrisMarquez <algrismarquez16@gmail.com>
2020-09-11 12:22:18 -0300

    cambios en css para organizar

--- src/components/MenuPostres/MenuPostres.js
+++ src/components/MenuPostres/MenuPostres.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import './MenuPostres.css'
+import React, { useEffect, useState, } from "react";
 
 function usePostres() {
     const [postres, setPostres] = useState([])
@@ -20,6 +19,7 @@ export default function Datoscuatro() {
             <table>
                 <tr>
                     <th><button id="" type="button" className="btnMenuTituloDos">POSTRES</button></th>
+               
                 </tr>
                 <tbody>
                     {postres.map(item => (
@@ -27,6 +27,7 @@ export default function Datoscuatro() {
                             <td><button id="" type="button" className="btnDetalleMenuDos">{item.name}:${item.price}</button></td>
                         </tr>
                     ))}
+                    
                 </tbody>
             </table>
         </div>
