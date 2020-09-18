0f2e7a27d99468cbb139b25e8d525127a35fdfe2
AlgrisMarquez <algrismarquez16@gmail.com>
2020-09-17 22:42:54 -0300

    cambios master mesero

--- src/view/viewMasterMesero.js
+++ src/view/viewMasterMesero.js
@@ -2,20 +2,11 @@ import React from 'react'
 
 import MenuLateral from '../components/MenuLateral/MenuLateral'
 import MenuPedido from '../components/MenuPedido/MenuPedido'
-<<<<<<< HEAD
-import MenuAlmuerzo from '../components/MenuAlmuerzo/MenuAlmuerzo'
-import MenuEnsaladas from '../components/MenuEnsaladas/MenuEnsaladas'
-import MenuPostres from '../components/MenuPostres/MenuPostres'
-import MenuLicores from '../components/MenuLicores/MenuLicores'
-import MenuBebidas from '../components/MenuBebidas/MenuBebidas'
-import Cart from '../components/Cart'
-=======
 
 import MenuMasterEnsaladas from '../components/MenuMasterEnsaladas/MenuMasterEnsaladas'
 
 
 
->>>>>>> 017204057b909a95cbfba22ab493b19d811ed628
 
 function PedidosMesero() {
     return(
@@ -24,12 +15,7 @@ function PedidosMesero() {
         <MenuMasterEnsaladas/>
         <MenuLateral/>
         <MenuPedido/>
-<<<<<<< HEAD
-        <MenuPostres/>
-        <Cart/>
-=======
       
->>>>>>> 017204057b909a95cbfba22ab493b19d811ed628
     </div> 
     )
 }
